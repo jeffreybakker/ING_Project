46ab06433494106c9672ad79403a741a48184d00
Jeffrey Bakker <jeffrey.bakker@gmail.com>
2017-03-13 22:34:16 +0100

    create a basic RESTful application

---
+++ src/main/java/honours/ing/banq/Application.java
@@ -0,0 +1,16 @@
+package honours.ing.banq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jeffrey Bakker
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/honours/ing/banq/Greeting.java
@@ -0,0 +1,23 @@
+package honours.ing.banq;
+
+/**
+ * @author Jeffrey Bakker
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
