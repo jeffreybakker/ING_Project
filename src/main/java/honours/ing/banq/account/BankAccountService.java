3e2979c9fe8d893a55a85db7d8ecc18dee1f1368
Jeffrey Bakker <jeffrey.bakker@gmail.com>
2017-04-17 01:41:01 +0200

    Implements the functionality for bank accounts like opening accounts, closing accounts and adding/removing holders

---
+++ src/main/java/honours/ing/banq/account/BankAccountService.java
@@ -0,0 +1,30 @@
+package honours.ing.banq.account;
+
+import com.googlecode.jsonrpc4j.JsonRpcParam;
+import com.googlecode.jsonrpc4j.JsonRpcService;
+import honours.ing.banq.customer.Customer;
+
+import java.util.List;
+
+/**
+ * @author jeffrey
+ * @since 17-4-17
+ */
+@JsonRpcService("/api/account")
+public interface BankAccountService {
+
+    BankAccount createAccount(@JsonRpcParam("holders") List<Customer> holders, @JsonRpcParam("balance") Double balance);
+
+    Double getBalance(@JsonRpcParam("id") Integer id);
+    Double setBalance(@JsonRpcParam("id") Integer id, @JsonRpcParam("balance") Double balance);
+
+    void deleteAccount(@JsonRpcParam("id") Integer id);
+
+    BankAccount findById(@JsonRpcParam("id") Integer id);
+    List<BankAccount> findByHolder(@JsonRpcParam("holderId") Integer id);
+
+    void addHolder(@JsonRpcParam("id") Integer id, @JsonRpcParam("holderId") Integer holderId);
+    List<Customer> listHolders(@JsonRpcParam("id") Integer id);
+    void removeHolder(@JsonRpcParam("id") Integer id, @JsonRpcParam("holderId") Integer holderId);
+
+}
