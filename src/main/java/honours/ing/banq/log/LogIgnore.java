a649b381960094b11f9c7b14185341ecb171b87f
Jeffrey Bakker <jeffrey.bakker@gmail.com>
2017-08-13 22:09:41 +0200

    add JavaDoc to the .log package

--- src/main/java/honours/ing/banq/log/LogIgnore.java
+++ src/main/java/honours/ing/banq/log/LogIgnore.java
@@ -6,7 +6,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * @author jeffrey
+ * All methods with this annotation will be ignored in the system logs.
+ * @author Jeffrey Bakker
  * @since 6-8-17
  */
 @Retention(RetentionPolicy.RUNTIME)
